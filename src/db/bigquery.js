1b613cd28e0f8c748593f801ac9a25596c1c2712
nk-the-crazy <nurkali@gmail.com>
2021-09-22 11:56:39 +0600

    Add active address queries and extract SQL queries into files

--- src/db/bigquery.js
+++ src/db/bigquery.js
@@ -1,23 +1,16 @@
 const { BigQuery } = require('@google-cloud/bigquery')
+const transactionStatsSQL = require('./sql/transactions_stats.sql')
+const addressRankSQL = require('./sql/address_rank.sql')
+const addressStatsSQL = require('./sql/address_stats.sql')
 
 const logger = require('../config/logger')
 
 async function getTransactions(fromDate) {
   const bigquery = new BigQuery()
-  const query = `
-    SELECT
-      date(block_timestamp) as date,
-      count(1) as count,
-      sum(value) / power(10, 18) as volume
-    from \`bigquery-public-data.crypto_ethereum.transactions\`
-    WHERE block_timestamp >= "${fromDate.toISOString()}"
-    GROUP BY date
-    ORDER BY date ASC
-  `
-
   const [job] = await bigquery.createQueryJob({
-    query,
-    location: 'US'
+    query: transactionStatsSQL,
+    location: 'US',
+    params: {from_date: fromDate.toISOString()},
   })
 
   logger.info(`Job ${job.id} started.`)
@@ -29,29 +22,18 @@ async function getTransactions(fromDate) {
   }))
 }
 
-async function getAddresses(fromDate) {
+
+// 2.7 GB query
+async function getTopAddresses(fromDate, recordCount) {
   const bigquery = new BigQuery()
-  const query = `
-    WITH entry_book AS (
-      SELECT
-        DISTINCT to_address AS address,
-        DATE(block_timestamp) as date
-      FROM \`bigquery-public-data.crypto_ethereum.traces\`
-      WHERE
-        to_address IS NOT NULL
-        AND status = 1
-        AND (call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR call_type IS NULL)
-        AND block_timestamp >= "${fromDate.toISOString()}"
-    )
-    SELECT COUNT(address) AS count, date
-    FROM entry_book
-    GROUP BY date
-    ORDER BY date DESC
-  `
 
   const [job] = await bigquery.createQueryJob({
-    query,
-    location: 'US'
+    query: addressRankSQL,
+    location: 'US',
+    params: {
+      from_date: fromDate.toISOString(),
+      record_count: recordCount
+    }
   })
 
   logger.info(`Job ${job.id} started.`)
@@ -62,8 +44,31 @@ async function getAddresses(fromDate) {
     date: item.date.value
   }))
 }
+
+// 2.7 GB query
+async function getAddressStats(fromDate) {
+  const bigquery = new BigQuery()
+
+  const [job] = await bigquery.createQueryJob({
+    query: addressStatsSQL,
+    location: 'US',
+    params: {
+      from_date: fromDate.toISOString()
+    }
+  })
+
+  logger.info(`Job ${job.id} started.`)
+
+  const [rows] = await job.getQueryResults()
+  return rows.map(item => ({
+    ...item,
+    date: item.date.value
+  }))
+}
+
 
 module.exports = {
   getTransactions,
-  getAddresses
+  getTopAddresses,
+  getAddressStats
 }
