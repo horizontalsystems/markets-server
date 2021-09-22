57230eb604359a40f3a1f8a92d5df0057fce93f2
nk-the-crazy <nurkali@gmail.com>
2021-09-22 12:02:19 +0600

    Correct code formatting

--- src/db/bigquery.js
+++ src/db/bigquery.js
@@ -1,74 +1,72 @@
-const { BigQuery } = require('@google-cloud/bigquery')
-const transactionStatsSQL = require('./sql/transactions_stats.sql')
-const addressRankSQL = require('./sql/address_rank.sql')
-const addressStatsSQL = require('./sql/address_stats.sql')
+const { BigQuery } = require("@google-cloud/bigquery");
+const transactionStatsSQL = require("./sql/transactions_stats.sql");
+const addressRankSQL = require("./sql/address_rank.sql");
+const addressStatsSQL = require("./sql/address_stats.sql");
 
-const logger = require('../config/logger')
+const logger = require("../config/logger");
 
 async function getTransactions(fromDate) {
-  const bigquery = new BigQuery()
+  const bigquery = new BigQuery();
   const [job] = await bigquery.createQueryJob({
     query: transactionStatsSQL,
-    location: 'US',
-    params: {from_date: fromDate.toISOString()},
-  })
+    location: "US",
+    params: { from_date: fromDate.toISOString() },
+  });
 
-  logger.info(`Job ${job.id} started.`)
+  logger.info(`Job ${job.id} started.`);
 
-  const [rows] = await job.getQueryResults()
-  return rows.map(item => ({
+  const [rows] = await job.getQueryResults();
+  return rows.map((item) => ({
     ...item,
-    date: item.date.value
-  }))
+    date: item.date.value,
+  }));
 }
-
 
 // 2.7 GB query
 async function getTopAddresses(fromDate, recordCount) {
-  const bigquery = new BigQuery()
+  const bigquery = new BigQuery();
 
   const [job] = await bigquery.createQueryJob({
     query: addressRankSQL,
-    location: 'US',
+    location: "US",
     params: {
       from_date: fromDate.toISOString(),
-      record_count: recordCount
-    }
-  })
+      record_count: recordCount,
+    },
+  });
 
-  logger.info(`Job ${job.id} started.`)
+  logger.info(`Job ${job.id} started.`);
 
-  const [rows] = await job.getQueryResults()
-  return rows.map(item => ({
+  const [rows] = await job.getQueryResults();
+  return rows.map((item) => ({
     ...item,
-    date: item.date.value
-  }))
+    date: item.date.value,
+  }));
 }
 
 // 2.7 GB query
 async function getAddressStats(fromDate) {
-  const bigquery = new BigQuery()
+  const bigquery = new BigQuery();
 
   const [job] = await bigquery.createQueryJob({
     query: addressStatsSQL,
-    location: 'US',
+    location: "US",
     params: {
-      from_date: fromDate.toISOString()
-    }
-  })
+      from_date: fromDate.toISOString(),
+    },
+  });
 
-  logger.info(`Job ${job.id} started.`)
+  logger.info(`Job ${job.id} started.`);
 
-  const [rows] = await job.getQueryResults()
-  return rows.map(item => ({
+  const [rows] = await job.getQueryResults();
+  return rows.map((item) => ({
     ...item,
-    date: item.date.value
-  }))
+    date: item.date.value,
+  }));
 }
-
 
 module.exports = {
   getTransactions,
   getTopAddresses,
-  getAddressStats
-}
+  getAddressStats,
+};
