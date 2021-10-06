61e66f6a74a276f54836ba6934a85d6966eacc07
nk-the-crazy <nurkali@gmail.com>
2021-10-06 14:35:36 +0600

    Update/Add models for address analytics

---
+++ src/db/models/AddressRank.js
@@ -0,0 +1,36 @@
+const SequelizeModel = require('./SequelizeModel')
+
+class AddressRank extends SequelizeModel {
+
+  static init(sequelize, DataTypes) {
+    return super.init(
+      {
+        address: {
+          type: DataTypes.STRING(100),
+          allowNull: false
+        },
+        volume: {
+          type: DataTypes.DECIMAL,
+          allowNull: false
+        }
+      },
+      {
+        sequelize,
+        tableName: 'address_ranks',
+      }
+    )
+  }
+
+  static associate(models) {
+    AddressRank.belongsTo(models.Platform, {
+      foreignKey: 'platform_id'
+    })
+  }
+
+  static deleteAll() {
+    return AddressRank.query('DELETE FROM address_ranks')
+  }
+
+}
+
+module.exports = AddressRank
