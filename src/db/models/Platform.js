6026681d5c4a85efa80217827665486858a7ec43
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-10-12 10:52:09 +0600

    Remove invalid query value

--- src/db/models/Platform.js
+++ src/db/models/Platform.js
@@ -63,7 +63,7 @@ class Platform extends SequelizeModel {
       FROM platforms 
       WHERE type = 'erc20'
         AND decimals IS NOT NULL
-        AND address = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+        AND address IS NOT NULL
     `)
   }
 }
