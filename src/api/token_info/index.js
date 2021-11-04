6849dee5eeab3cba69f85b91debe2a8d99642ef9
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-11-04 17:48:27 +0600

    Add documentation to API `/v1/token_info/:platform`

--- src/api/token_info/index.js
+++ src/api/token_info/index.js
@@ -3,6 +3,27 @@ const controller = require('./token_info.controller')
 
 const router = express.Router()
 
+/**
+ * @api {get} /v1/token_info/:platform Get Token info
+ * @apiDescription Get Token information
+ * @apiVersion 1.0.0
+ * @apiGroup Token
+ *
+ * @apiParam  {String=erc20,bep20,bep2}    platform  Coin's platform
+ * @apiParam  {String}                  address   Coin's contract address
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *  HTTP/1.1 200 OK
+ *  [{
+ *    "decimals": 18,
+ *    "name": "Super Token",
+ *    "symbol": "SUP"
+ *  }]
+ *
+ * @apiError (Bad Request 400)  ValidationError   Some parameters may contain invalid values
+ * @apiError (Not Found 404)    NotFound          Not found
+ */
+
 router.get('/:type', controller.info)
 
 module.exports = router
