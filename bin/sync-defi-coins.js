08f2f30882dbd0448e230d869dcb3ce79d65d0a7
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-11-15 09:40:47 +0600

    Move defi-coin syncer to separate script

--- bin/sync-defi-coins.js
+++ bin/sync-defi-coins.js
@@ -1,13 +1,13 @@
 const sequelize = require('../src/db/sequelize')
 const logger = require('../src/config/logger')
 
-const CoinSyncer = require('../src/services/CoinSyncer')
+const DefiCoinSyncer = require('../src/services/DefiCoinSyncer')
 
 async function start() {
   await sequelize.sync()
-  const coinSyncer = new CoinSyncer()
+  const defiCoinSyncer = new DefiCoinSyncer()
 
-  await coinSyncer.start()
+  await defiCoinSyncer.start()
 }
 
 start().catch(err => {
