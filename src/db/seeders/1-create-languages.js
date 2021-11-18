0ec4c16b33e3c0ae798d2f38105cccc4fc57caad
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-11-18 14:41:05 +0600

    Update seeds

--- src/db/seeders/1-create-languages.js
+++ src/db/seeders/1-create-languages.js
@@ -1,15 +1,15 @@
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('languages', [
-      { id: 1, code: 'en', name: 'English' },
-      { id: 2, code: 'de', name: 'German' },
-      { id: 3, code: 'ru', name: 'Russian' },
-      { id: 4, code: 'es', name: 'Spanish' },
-      { id: 5, code: 'fa', name: 'Persian' },
-      { id: 6, code: 'fr', name: 'French' },
-      { id: 7, code: 'ko', name: 'Korean' },
-      { id: 8, code: 'tr', name: 'Turkish' },
-      { id: 9, code: 'zh', name: 'Chinese' }
+      { code: 'en', name: 'English' },
+      { code: 'de', name: 'German' },
+      { code: 'ru', name: 'Russian' },
+      { code: 'es', name: 'Spanish' },
+      { code: 'fa', name: 'Persian' },
+      { code: 'fr', name: 'French' },
+      { code: 'ko', name: 'Korean' },
+      { code: 'tr', name: 'Turkish' },
+      { code: 'zh', name: 'Chinese' }
     ], {})
   },
 
