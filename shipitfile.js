14cb37e591385be711ac34978ac870c5a65e58e6
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-12-01 12:30:58 +0600

    Add deploy scripts

---
+++ shipitfile.js
@@ -0,0 +1,77 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const deploy = require('shipit-deploy')
+const shared = require('shipit-shared')
+const { apps } = require('./process.json')
+
+module.exports = shipit => {
+  deploy(shipit)
+  shared(shipit)
+
+  shipit.initConfig({
+    default: {
+      deployTo: '/home/deploy/markets-server',
+      keepReleases: 5,
+      repositoryUrl: 'https://github.com/horizontalsystems/markets-server.git',
+      shared: {
+        overwrite: true,
+        files: ['.env'],
+        dirs: ['node_modules']
+      }
+    },
+    production: {
+      branch: 'master',
+      servers: ['deploy@147.182.182.176'],
+    },
+    development: {
+      branch: 'develop',
+      servers: ['deploy@147.182.182.176']
+    }
+  })
+
+  const remote = cmd => {
+    shipit.remote(`cd ${shipit.config.deployTo}/current && ${cmd}`)
+  }
+
+  apps.forEach(({ name: app }) => {
+    shipit.blTask(`${app}:start`, () => {
+      shipit.start(`${app}:restart`)
+    })
+
+    shipit.blTask(`${app}:stop`, () => {
+      remote(`pm2 stop process.json --only ${app}`)
+    })
+
+    shipit.blTask(`${app}:restart`, () => {
+      remote(`pm2 startOrRestart process.json --env production --only ${app}`)
+    })
+
+    shipit.blTask(`${app}:logs`, () => {
+      remote(`pm2 logs ${app}`)
+    })
+  })
+
+  shipit.on('deployed', () => shipit.start('npm:install'))
+  shipit.on('rollback', () => shipit.start('npm:install'))
+
+  shipit.blTask('npm:install', async () => {
+    remote('npm install --production')
+  })
+
+  shipit.blTask('copy-env', () => {
+    shipit.copyToRemote('.env', `${shipit.config.deployTo}/.env`)
+  })
+
+  shipit.blTask('setup:node', async () => {
+    // enable interactive mode in .bashrc
+    // # case $- in
+    // #     *i*) ;;
+    // #       *) return;;
+    // # esac
+
+    await shipit.remote('curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash')
+    await shipit.remote('nvm install --lts')
+    await shipit.remote('nvm install-latest-npm')
+    await shipit.remote('npm install pm2 -g')
+  })
+}
