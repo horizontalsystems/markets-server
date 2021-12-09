472075a40f546a8ef840d3881e56fb124d2468f8
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-12-09 12:51:36 +0600

    Simplify bigquery provider

--- src/providers/bigquery.js
+++ src/providers/bigquery.js
@@ -2,7 +2,6 @@ const { BigQuery } = require('@google-cloud/bigquery')
 const { requireFile } = require('../utils')
 const logger = require('../config/logger')
 
-const bigquery = new BigQuery()
 const transactionStatsSQL = requireFile('providers/bigquery-sql/transaction_stats.sql')
 const addressRankSQL = requireFile('providers/bigquery-sql/address_rank.sql')
 const addressStatsSQL = requireFile('providers/bigquery-sql/address_stats.sql')
@@ -23,113 +22,67 @@ const dexLiquidity = {
   uniswap_v3_bydate: requireFile('providers/bigquery-sql/uniswap_v3_liquidity_bydate.sql')
 }
 
-exports.getTransactionsStats = async (dateFrom, dateTo, tokens, period) => {
-  const [job] = await bigquery.createQueryJob({
-    query: transactionStatsSQL,
-    location: 'US',
-    params: {
+class BigQueryClient extends BigQuery {
+
+  async createQuery(query, params) {
+    const [job] = await this.createQueryJob({ query, params, location: 'US' })
+    logger.info(`Job ${job.id} started.`)
+
+    const [rows] = await job.getQueryResults()
+    return rows
+  }
+
+  getTransactionsStats(dateFrom, dateTo, tokens, period) {
+    return this.createQuery(transactionStatsSQL, {
       dateFrom,
       dateTo,
       period,
       supported_tokens: tokens
-    }
-  })
+    })
+  }
 
-  logger.info(`Job ${job.id} started.`)
-
-  const [rows] = await job.getQueryResults()
-  return rows
-}
-
-exports.getDexLiquidity = async (dateFrom, dateTo, period, tokens, queryType) => {
-  const query = dexLiquidity[queryType]
-  const [job] = await bigquery.createQueryJob({
-    query,
-    location: 'US',
-    params: {
+  getDexLiquidity(dateFrom, dateTo, period, tokens, queryType) {
+    return this.createQuery(dexLiquidity[queryType], {
       period,
       date_from: dateFrom,
       date_to: dateTo,
       supported_tokens: tokens
-    }
-  })
+    })
+  }
 
-  logger.info(`Job ${job.id} started.`)
-
-  const [rows] = await job.getQueryResults()
-  return rows
-}
-
-exports.getDexVolumes = async (dateFrom, dateTo, tokens, period, queryType) => {
-  const query = dexVolume[queryType]
-  const [job] = await bigquery.createQueryJob({
-    query,
-    location: 'US',
-    params: {
+  getDexVolumes(dateFrom, dateTo, tokens, period, queryType) {
+    return this.createQuery(dexVolume[queryType], {
       dateFrom,
       dateTo,
       period,
       supported_tokens: tokens
-    }
-  })
+    })
+  }
 
-  logger.info(`Job ${job.id} started.`)
-
-  const [rows] = await job.getQueryResults()
-  return rows
-}
-
-// 2.7 GB query
-exports.getTopCoinHolders = async (tokens, dateFrom, addressesPerCoin) => {
-  const [job] = await bigquery.createQueryJob({
-    query: coinHoldersSQL,
-    location: 'US',
-    params: {
+  getTopCoinHolders(tokens, dateFrom, addressesPerCoin) {
+    return this.createQuery(coinHoldersSQL, {
       supported_tokens: tokens,
       date_from: dateFrom,
       addresses_per_coin: addressesPerCoin
-    }
-  })
+    })
+  }
 
-  logger.info(`Job ${job.id} for CoinHolders started.`)
-
-  const [rows] = await job.getQueryResults()
-  return rows
-}
-
-// 2.7 GB query , for 3 month
-exports.getTopAddresses = async (tokens, dateFrom, addressesPerCoin) => {
-  const [job] = await bigquery.createQueryJob({
-    query: addressRankSQL,
-    location: 'US',
-    params: {
+  getTopAddresses(tokens, dateFrom, addressesPerCoin) {
+    return this.createQuery(addressRankSQL, {
       supported_tokens: tokens,
       date_from: dateFrom,
       addresses_per_coin: addressesPerCoin
-    }
-  })
+    })
+  }
 
-  logger.info(`Job ${job.id} for AddressRanks started.`)
-
-  const [rows] = await job.getQueryResults()
-  return rows
-}
-
-// 2 GB query for 3 month
-exports.getAddressStats = async (tokens, dateFrom, dateTo, timePeriod) => {
-  const [job] = await bigquery.createQueryJob({
-    query: addressStatsSQL,
-    location: 'US',
-    params: {
+  getAddressStats(tokens, dateFrom, dateTo, timePeriod) {
+    return this.createQuery(addressStatsSQL, {
       supported_tokens: tokens,
       period: timePeriod,
       date_from: dateFrom,
       date_to: dateTo,
-    }
-  })
-
-  logger.info(`Job ${job.id} for AddressStats started.`)
-
-  const [rows] = await job.getQueryResults()
-  return rows
+    })
+  }
 }
+
+module.exports = new BigQueryClient()
