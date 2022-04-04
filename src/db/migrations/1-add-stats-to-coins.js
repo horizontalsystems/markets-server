386a3e48df83039f82d0f2ee5526d52641921d36
Talgat Medetbekov <medetbekov.t@gmail.com>
2022-04-04 15:57:32 +0600

    Cleanup migrations

--- src/db/migrations/1-add-stats-to-coins.js
+++ src/db/migrations/1-add-stats-to-coins.js
@@ -1,8 +1,7 @@
 module.exports = {
   up(query, Sequelize) {
     return query.addColumn('coins', 'stats', {
-      type: Sequelize.JSONB,
-      defaultValue: {}
+      type: Sequelize.JSONB
     })
   },
 
