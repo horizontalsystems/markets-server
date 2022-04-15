3c0de44e0e5ea50af5236c645467a95202c921f9
nk-the-crazy <nurkali@gmail.com>
2022-04-15 12:37:17 +0600

    Fix migration script for addresses table

---
+++ src/db/migrations/3-add-data-to-address.js
@@ -0,0 +1,33 @@
+module.exports = {
+  async up(query, Sequelize) {
+    const transaction = await query.sequelize.transaction()
+
+    try {
+      query.removeColumn('addresses', 'count')
+      await query.addColumn('addresses', 'data', {
+        type: Sequelize.JSONB,
+        defaultValue: {}
+      })
+
+      await transaction.commit()
+    } catch (err) {
+      await transaction.rollback()
+      throw err
+    }
+  },
+
+  async down(query, Sequelize) {
+    const transaction = await query.sequelize.transaction()
+    try {
+      await query.removeColumn('addresses', 'data')
+      await query.addColumn('addresses', 'count', {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      })
+      await transaction.commit()
+    } catch (err) {
+      await transaction.rollback()
+      throw err
+    }
+  }
+}
