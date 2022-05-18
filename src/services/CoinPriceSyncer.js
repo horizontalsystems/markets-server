48723c5b923b60ffcf716a98630c20cd3527219a
Talgat Medetbekov <medetbekov.t@gmail.com>
2022-05-18 15:24:46 +0600

    Correctly pass coins to query filter

--- src/services/CoinPriceSyncer.js
+++ src/services/CoinPriceSyncer.js
@@ -28,7 +28,7 @@ class CoinPriceSyncer extends CoinPriceHistorySyncer {
 
   async sync(uid) {
     const where = {
-      ...{ uid } && uid,
+      ...(uid && { uid }),
       coingecko_id: Coin.literal('coingecko_id IS NOT NULL')
     }
     const coins = await Coin.findAll({ attributes: ['id', 'coingecko_id'], where })
