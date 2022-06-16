b0b43110464cd5fe865a95c88d09b2e9a701e119
Talgat Medetbekov <medetbekov.t@gmail.com>
2022-06-16 15:39:20 +0600

    Remove redundant /v1/chain route

--- src/config/routes.js
+++ src/config/routes.js
@@ -23,7 +23,6 @@ router.use('/reports', require('../api/reports'))
 router.use('/markets', require('../api/global-markets'))
 router.use('/global-markets', require('../api/global-markets'))
 router.use('/evm-method-labels', require('../api/evm-method-labels'))
-router.use('/chain', require('../api/chain'))
 router.use('/status', require('../api/status'))
 
 module.exports = router
