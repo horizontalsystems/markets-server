987b117fc833194ae2a3b5ec3e3e7e7c2bd8492b
Talgat Medetbekov <medetbekov.t@gmail.com>
2022-06-16 14:50:00 +0600

    Add url to Chain.js

--- src/db/models/Chain.js
+++ src/db/models/Chain.js
@@ -14,6 +14,7 @@ class Chain extends SequelizeModel {
           type: DataTypes.STRING,
           allowNull: false
         },
+        url: DataTypes.STRING,
         stats: DataTypes.JSONB,
         //  {
         //    market_cap: 99,
