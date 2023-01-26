196c0e8d011716ef5c9250ce181b5aa0ff82c843
Talgat Medetbekov <medetbekov.t@gmail.com>
2023-01-26 11:49:35 +0600

    Add ability to start specific syncers

--- scripts/sync-dex-data.js
+++ scripts/sync-dex-data.js
@@ -1,27 +1,40 @@
 require('dotenv/config')
 
+const { Command } = require('commander')
 const sequelize = require('../src/db/sequelize')
 const AddressSyncer = require('../src/services/AddressSyncer')
 const TransactionSyncer = require('../src/services/TransactionSyncer')
 const DexVolumeSyncer = require('../src/services/DexVolumeSyncer')
 const DexLiquiditySyncer = require('../src/services/DexLiquiditySyncer')
 
-async function start() {
+const program = new Command()
+  .option('-t --tx', 'sync transactions only')
+  .option('-v --volume', 'sync dex volumes only')
+  .option('-l --liquidity', 'sync dex liquidity only')
+  .option('-a --address', 'sync addresses only')
+  .parse(process.argv)
+
+async function start({ tx, volume, liquidity, address }) {
   await sequelize.sync()
   const transactionSyncer = new TransactionSyncer()
   const dexVolumeSyncer = new DexVolumeSyncer()
   const dexLiquiditySyncer = new DexLiquiditySyncer()
   const addressSyncer = new AddressSyncer()
+  const syncers = []
 
-  await Promise.all([
-    transactionSyncer.start(),
-    dexVolumeSyncer.start(),
-    dexLiquiditySyncer.start(),
-    addressSyncer.start()
-  ]).catch(e => {
+  if (tx) syncers.push(transactionSyncer)
+  if (address) syncers.push(addressSyncer)
+  if (liquidity) syncers.push(dexLiquiditySyncer)
+  if (volume) syncers.push(dexVolumeSyncer)
+
+  if (!syncers.length) {
+    syncers.push(transactionSyncer, dexVolumeSyncer, dexLiquiditySyncer, addressSyncer)
+  }
+
+  await Promise.all(syncers.map(s => s.start())).catch(e => {
     console.log(e.message)
     throw e
   })
 }
 
-module.exports = start()
+module.exports = start(program.opts())
