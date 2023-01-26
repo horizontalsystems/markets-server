43028df0029f5d21bc5e3b486e99369197d8c8d5
Talgat Medetbekov <medetbekov.t@gmail.com>
2023-01-26 11:52:18 +0600

    Add ability to start specific stats syncers

--- scripts/sync-stats.js
+++ scripts/sync-stats.js
@@ -1,19 +1,36 @@
 require('dotenv/config')
 
+const { Command } = require('commander')
 const sequelize = require('../src/db/sequelize')
 const CategoryMarketCapSyncer = require('../src/services/CategoryMarketCapSyncer')
 const TopPlatformsSyncer = require('../src/services/TopPlatformsSyncer')
-// const CoinRatingSyncer = require('../src/services/CoinRatingSyncer')
+const CoinRatingSyncer = require('../src/services/CoinRatingSyncer')
 
-async function start() {
+const program = new Command()
+  .option('-c --category', 'sync top categories only')
+  .option('-r --rating', 'sync coin ratings only')
+  .option('-p --platforms', 'sync top platforms only')
+  .parse(process.argv)
+
+async function start({ category, rating, platforms }) {
   await sequelize.sync()
   const categoryMarketCapSyncer = new CategoryMarketCapSyncer()
-  // const coinRatingSyncer = new CoinRatingSyncer()
+  const coinRatingSyncer = new CoinRatingSyncer()
   const topPlatformsSyncer = new TopPlatformsSyncer()
+  const syncers = []
 
   try {
+
+    if (category) syncers.push(categoryMarketCapSyncer)
+    if (rating) syncers.push(coinRatingSyncer)
+    if (platforms) syncers.push(topPlatformsSyncer)
+
+    if (!syncers.length) {
+      syncers.push(categoryMarketCapSyncer, coinRatingSyncer, topPlatformsSyncer)
+    }
+
     await categoryMarketCapSyncer.start()
-    // await coinRatingSyncer.start()
+    await coinRatingSyncer.start()
     await topPlatformsSyncer.start()
   } catch (e) {
     console.log(e.message)
@@ -21,4 +38,4 @@ async function start() {
   }
 }
 
-module.exports = start()
+module.exports = start(program.opts())
