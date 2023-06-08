0b79e520a3dfdea476ee2a71d7033b4226df74ae
Talgat Medetbekov <medetbekov.t@gmail.com>
2023-06-08 12:39:28 +0600

    Revert "Use typed structured data hashing (Eip712) for authentication"
    This reverts commit d9e92decfb9dc572de1a2fdf3c65aa2f3ab44978.

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -5,33 +5,22 @@ const { validateAuthenticate, validateAuthKey } = require('./auth.validator')
 const router = express.Router()
 
 /**
- * @api {get} /v1/auth/get-eip712-data Get EIP-712 data
- * @apiDescription Get EIP-712 message data
+ * @api {get} /v1/auth/get-key Get auth key
+ * @apiDescription Get a random key for signature
  * @apiVersion 1.0.0
  * @apiGroup Auth
+ *
+ * @apiParam    {String}  address Wallet address
+ *
+ * @apiSuccess  {String}  key Random key
  *
  * @apiSuccessExample {json} Success-Response:
  *  HTTP/1.1 200 OK
  *  {
- *    types: {
- *      EIP712Domain: [
- *        { name: 'name', type: 'string' },
- *        { name: 'version', type: 'string' },
- *      ],
- *      AccessRequest: [
- *      ]
- *    },
- *    domain: {
- *      name: 'Unstoppable Wallet Premium Subscription',
- *      version: '1',
- *      verifyingContract: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
- *    },
- *    primaryType: 'AccessRequest',
- *    message: {
- *    }
+ *    "key": "abc"
  *  }
  */
-router.get('/get-eip712-data', controller.getEip712Data)
+router.get('/get-key', validateAuthKey, controller.generateKey)
 
 /**
  * @api {post} /v1/auth/authenticate Authenticate
