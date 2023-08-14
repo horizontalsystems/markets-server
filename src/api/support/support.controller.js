9c0f86b14a1cc623e05ff595cea09ea152e7a96f
Talgat Medetbekov <medetbekov.t@gmail.com>
2023-08-14 15:28:22 +0600

    Require username in support chat API

--- src/api/support/support.controller.js
+++ src/api/support/support.controller.js
@@ -1,23 +1,13 @@
 const telegram = require('../../providers/telegram')
-const Subscription = require('../../db/models/Subscription')
 const { handleError } = require('../middlewares')
 
-exports.startChat = async ({ body, address }, res) => {
+exports.startChat = async ({ body }, res) => {
   try {
-    const subscription = await Subscription.findOne({
-      where: { address }
-    })
-
-    if (!subscription) {
-      return handleError(res, 403, 'Subscription is required')
-    }
-
-    if (subscription.chat_started) {
-      // return handleError(res, 403, 'Already sent a message')
+    if (!body.username) {
+      return handleError(res, 403, 'Username is required')
     }
 
     await telegram.sendMessage(body.username)
-    await subscription.update({ chat_started: true })
     res.send({})
   } catch (e) {
     console.log(e)
