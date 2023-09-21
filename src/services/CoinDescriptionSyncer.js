61a2385d19ef08109e9a1d9ca3b43d71ea78efa7
Talgat Medetbekov <medetbekov.t@gmail.com>
2023-09-21 17:49:33 +0600

    Sync descriptions from Bard

--- src/services/CoinDescriptionSyncer.js
+++ src/services/CoinDescriptionSyncer.js
@@ -1,3 +1,5 @@
+const { TextServiceClient } = require('@google-ai/generativelanguage').v1beta2
+const { GoogleAuth } = require('google-auth-library')
 const OpenAI = require('openai')
 const utils = require('../utils')
 const Coin = require('../db/models/Coin')
@@ -5,6 +7,10 @@ const Coin = require('../db/models/Coin')
 const { chat } = new OpenAI({
   organization: process.env.CHAT_GPT_ORG,
   apiKey: process.env.CHAT_GPT_KEY
+})
+
+const palm = new TextServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(process.env.PALM_KEY),
 })
 
 class CoinDescriptionSyncer {
@@ -19,30 +25,54 @@ class CoinDescriptionSyncer {
     for (let i = 0; i < coins.ids.length; i += 1) {
       const uid = coins.ids[i]
       await this.syncDescription(uid, coins.map[uid])
-      await utils.sleep(300)
+      await utils.sleep(500)
     }
   }
 
   async syncDescription(uid, coin) {
-    console.log(`Syncing descriptions for ${uid}`)
+    console.log(`Syncing descriptions for ${uid}`, coin)
+
+    const palmDesc = await this.getDescriptionFromPalm(JSON.stringify({ [uid]: coin.overview }))
+    const gptDesc = await this.getDescriptionFromGPT(JSON.stringify({ [uid]: palmDesc || coin.overview }))
+
+    await this.updateDescription(coin, gptDesc)
+  }
+
+  async getDescriptionFromGPT(content) {
+    console.log('Fetching data from GPT')
 
     const { choices = [] } = await chat.completions.create({
-      messages: [{
-        role: 'system',
-        content: `
-You're an educator and expert with in-depth knowledge of cryptocurrency and blockchain fields. One of your core strengths lies in your ability to break down complex subjects into simple concepts that people without specialized knowledge can understand. 
-You will receive a list of cryptocurrencies in a key-value format, where the key represents the ID and the value represents its description. Your task is to obtain a good and lengthy overview of each project that captures its key points. The summary should explain not only the project but also the purpose behind it i.e. what problem the project aims to solve, the actors involved, etc. Try to break it down into many paragraphs so it's easier to follow and use the descriptions as a reference.
-These overviews are primarily aimed at people looking to invest in the respective projects. In a way, the description will be used for not only educational purposes but also as a means to evaluate the project's validity. Avoid using technical terms not known to people not involved with blockchain technologies.
-Always format the response in the Markdown. Return null if delivering an overview is not possible.`
-      }, {
-        role: 'user',
-        content: JSON.stringify({ [uid]: coin.overview })
-      }],
-      model: 'gpt-4-0613',
+      model: 'gpt-4',
+      messages: [
+        { role: 'system', content: utils.getGptPrompt() },
+        { role: 'user', content }
+      ]
     })
 
     const { message = {} } = choices[0] || {}
-    await this.updateDescription(coin, message)
+
+    return message
+  }
+
+  async getDescriptionFromPalm(prompt) {
+    console.log('Fetching data from PaLM')
+
+    const text = `
+      ${utils.getGptPrompt()}
+      ${prompt}`
+
+    const [result] = await palm.generateText({
+      model: 'models/text-bison-001',
+      prompt: { text }
+    })
+
+    if (!result || !result.candidates || !result.candidates.length) {
+      return null
+    }
+
+    const candidate = result.candidates[0]
+
+    return candidate.output
   }
 
   async updateDescription(coin, { content }) {
