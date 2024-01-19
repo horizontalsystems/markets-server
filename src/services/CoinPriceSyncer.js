bf31bc23c18f7519883849ca30c89e3b99b1f4e4
Talgat Medetbekov <medetbekov.t@gmail.com>
2024-01-19 15:29:22 +0600

    Increase wait time to 12000ms

--- src/services/CoinPriceSyncer.js
+++ src/services/CoinPriceSyncer.js
@@ -45,7 +45,7 @@ class CoinPriceSyncer extends CoinPriceHistorySyncer {
     try {
       const data = await coingecko.getSimplePrices(uids)
       await this.storeSimplePrices(Object.entries(data), idsMap)
-      await utils.sleep(9000)
+      await utils.sleep(12000)
     } catch (e) {
       await this.handleHttpError(e)
     }
