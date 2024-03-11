cd5db299960ff4b7b4fa6bcd6b7359e92e77f32f
Talgat Medetbekov <medetbekov.t@gmail.com>
2024-03-11 16:10:10 +0600

    Add stats API

--- src/api/stats/index.js
+++ src/api/stats/index.js
@@ -4,6 +4,26 @@ const controller = require('./stats.controller')
 const router = express.Router()
 
 /**
+ * @api {head} /v1/stats Pass page stats
+ * @apiDescription Pass page stats
+ * @apiVersion 1.0.0
+ * @apiGroup Stats
+ *
+ * @apiParam  {String}  tag_name        Tag name
+ * @apiParam  {String}  [tag_type]      Tag type
+ * @apiParam  {String}  [tag_parent]    Tag parent
+ * @apiParam  {String}  [tag_from]      Tag from
+ * @apiParam  {String}  [coin_uid]      Coin UID
+ * @apiParam  {String}  [oscillators]   Oscillators
+ * @apiParam  {String}  [ma]            Moving averages
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *  HTTP/1.1 200 OK
+ *  {}
+ */
+router.head('/', controller.analytics)
+
+/**
  * @api {get} /v1/stats/popular-coins Get popular coins
  * @apiDescription Get popular coins
  * @apiVersion 1.0.0
--- src/api/stats/stats.controller.js
+++ src/api/stats/stats.controller.js
@@ -10,3 +10,87 @@ exports.popularResources = async (req, res) => {
   const resources = await mongo.getStats('resource_stats')
   res.send(serializer.serializeResources(resources))
 }
+
+exports.analytics = async ({ query }, res) => {
+  const stats = {
+    tag: query.tag_name,
+  }
+
+  if (query.tag_type) stats.type = query.tag_type
+  if (query.tag_parent) stats.parent = query.tag_parent
+  if (query.tag_from) stats.from = query.tag_from
+
+  // indicators
+  if (query.ma) stats.ma = query.ma
+  if (query.oscillators) stats.oscillators = query.oscillators
+
+  // coins
+  if (query.coin_uid) stats.coin_uid = query.coin_uid
+
+  try {
+    await mongo.storeStats(stats)
+  } catch (e) {
+    console.log(e.message)
+  }
+
+  res.end()
+}
+
+exports.logs = async () => {
+  await mongo.storeStats({
+    tag_name: 'market',
+    tag_type: 'page_view',
+  })
+
+  await mongo.storeStats({
+    tag_name: 'coin_page',
+    tag_type: 'page_view',
+    tag_parent: 'market',
+  })
+
+  await mongo.storeStats({
+    tag_name: 'enable_coin',
+    tag_type: 'click_add_button',
+    tag_parent: 'coin_page',
+    coin_uid: 'uniswap'
+  })
+
+  await mongo.storeStats({
+    tag_name: 'indicators',
+    tag_type: 'page_view',
+    tag_parent: 'coin_page',
+  })
+
+  await mongo.storeStats({
+    tag_name: 'indicators_settings',
+    tag_parent: 'indicators',
+    tag_data: {
+      moving_averages: ['ma1'],
+      oscillators: []
+    },
+  })
+
+  await mongo.storeStats({
+    tag_name: 'indicators',
+    tag_type: 'page_view',
+    tag_parent: 'coin_page',
+    event_name: 'featured_product_click',
+    event_data: {
+      button_id: 'btn_featured_product',
+      filter_type: 'price',
+      filter_value: '$100 - $200'
+    }
+  })
+
+  const data = {
+    page_name: 'homepage',                // Name or identifier of the page
+    page_category: 'landing',             // Category of the page (optional)
+    event_type: 'page_view',              // Type of event: page_view, button_click, filter_chosen, etc.
+    event_name: 'featured_product_click', // Name or identifier of the event (optional)
+    event_data: {                         // Additional data related to the event (optional)
+      button_id: 'btn_featured_product',
+      filter_type: 'price',
+      filter_value: '$100 - $200'
+    }
+  }
+}
