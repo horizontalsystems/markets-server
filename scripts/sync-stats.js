9677b67ca2bce8840e9ddba7b5ad90ed29e52165
Talgat Medetbekov <medetbekov.t@gmail.com>
2024-04-15 16:57:07 +0600

    Remove unnecessary stats syncer

--- scripts/sync-stats.js
+++ scripts/sync-stats.js
@@ -5,38 +5,34 @@ const sequelize = require('../src/db/sequelize')
 const CategoryMarketCapSyncer = require('../src/services/CategoryMarketCapSyncer')
 const TopPlatformsSyncer = require('../src/services/TopPlatformsSyncer')
 const CoinRankSyncer = require('../src/services/CoinRankSyncer')
-const PopularStatsSyncer = require('../src/services/PopularStatsSyncer')
 const IndicatorSyncer = require('../src/services/IndicatorSyncer');
 
 const program = new Command()
   .option('-c --category', 'sync top categories only')
   .option('-r --rating', 'sync coin ratings only')
   .option('-p --platforms', 'sync top platforms only')
-  .option('-s --stats', 'sync popular stats')
   .option('-i --indicator', 'sync indicators')
   .option('--coins <coins>', 'run syncer for the given coins')
   .option('--rank', 'sync coins rank only')
   .option('-f --force', 'force sync')
   .parse(process.argv)
 
-async function start({ category, platforms, rank, force, stats, indicator, coins }) {
+async function start({ category, platforms, rank, force, indicator, coins }) {
   await sequelize.sync()
   const categoryMarketCapSyncer = new CategoryMarketCapSyncer()
   const coinRankSyncer = new CoinRankSyncer()
   const topPlatformsSyncer = new TopPlatformsSyncer()
   const indicatorSyncer = new IndicatorSyncer()
-  const popularStatsSyncer = new PopularStatsSyncer()
   const syncers = []
 
   try {
     if (category) syncers.push(categoryMarketCapSyncer)
     if (rank) syncers.push(coinRankSyncer)
     if (platforms) syncers.push(topPlatformsSyncer)
-    if (stats) syncers.push(popularStatsSyncer)
     if (indicator) syncers.push(indicatorSyncer)
 
     if (!syncers.length) {
-      syncers.push(categoryMarketCapSyncer, coinRankSyncer, topPlatformsSyncer, popularStatsSyncer, indicatorSyncer)
+      syncers.push(categoryMarketCapSyncer, coinRankSyncer, topPlatformsSyncer, indicatorSyncer)
     }
 
     const mapper = s => {
