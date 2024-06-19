769c82e1ba03ec9a97be982c4cdc43d6d72435fd
Talgat Medetbekov <medetbekov.t@gmail.com>
2024-06-19 15:37:36 +0600

    Get active token-unlock events only

--- src/api/token-unlocks/token-unlocks.controller.js
+++ src/api/token-unlocks/token-unlocks.controller.js
@@ -2,6 +2,6 @@ const serializer = require('./token-unlocks.serializer')
 const TokenUnlock = require('../../db/models/TokenUnlock')
 
 exports.index = async (req, res) => {
-  const unlocks = await TokenUnlock.query('select * from token_unlocks order by date asc')
+  const unlocks = await TokenUnlock.query('select * from token_unlocks where date >= NOW() order by date asc')
   res.send(serializer.serializeIndex(unlocks))
 }
