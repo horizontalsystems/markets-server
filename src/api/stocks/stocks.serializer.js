06f95777ed0d31017c585ff33bcc90e9b55ee3c2
Talgat Medetbekov <medetbekov.t@gmail.com>
2025-07-07 12:49:54 +0600

    Add all stock price changes to API

--- src/api/stocks/stocks.serializer.js
+++ src/api/stocks/stocks.serializer.js
@@ -8,11 +8,15 @@ exports.serializeIndex = (items) => {
       symbol: item.symbol,
       market_price: nullOrString(item.market_price),
       price_change: {
-        '1y': nullOrString(item.price_change['1y']),
+        '1d': nullOrString(item.price_change['1d']),
         '7d': nullOrString(item.price_change['7d']),
         '30d': nullOrString(item.price_change['30d']),
         '90d': nullOrString(item.price_change['90d']),
-        '200d': nullOrString(item.price_change['200d'])
+        '200d': nullOrString(item.price_change['200d']),
+        '1y': nullOrString(item.price_change['1y']),
+        '2y': nullOrString(item.price_change['2y']),
+        '3y': nullOrString(item.price_change['3y']),
+        '4y': nullOrString(item.price_change['4y']),
       }
     }
   })
