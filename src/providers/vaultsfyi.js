372f10777deaf99dff6e8f060e049991b9d4263d
Talgat Medetbekov <medetbekov.t@gmail.com>
2025-07-22 17:25:49 +0600

    Specify chains when syncing vaults

--- src/providers/vaultsfyi.js
+++ src/providers/vaultsfyi.js
@@ -3,9 +3,14 @@ const { create } = require('axios')
 
 const axios = create({
   baseURL: 'https://app.vaults.fyi/api/proxy',
+  timeout: 180000 * 3
+})
+
+const api = create({
+  baseURL: 'https://api.vaults.fyi/api',
   timeout: 180000 * 3,
   headers: {
-    // 'x-api-key': process.env.VAULTSFYI_KEY
+    'x-api-key': process.env.VAULTSFYI_KEY
   }
 })
 
@@ -13,11 +18,15 @@ exports.getAllVaults = (page, perPage = 5000) => {
   const params = {
     page,
     perPage,
-    minTvl: 1000000,
+    minTvl: 100000,
     onlyAppFeatured: true
   }
 
-  return axios.get(`/v2/detailed-vaults?${stringify(params)}`)
+  const chains = [
+    'mainnet', 'optimism', 'arbitrum', 'polygon', 'gnosis', 'base', 'unichain', 'swellchain', 'celo', 'worldchain', 'berachain', 'ink', 'bsc'
+  ].join('&allowedNetworks=')
+
+  return axios.get(`/v2/detailed-vaults?${stringify(params)}&allowedNetworks=${chains}`)
     .then(res => res.data)
     .then(res => res.data)
 }
